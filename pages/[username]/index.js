f7e0970261dba256dc42431c7c1cd07deb6882e8
Alex Blackwell <blackwellalex0@gmail.com>
2021-08-05 14:52:41 -0400

    routing the app

---
+++ pages/[username]/index.js
@@ -0,0 +1,7 @@
+export default function EditPortfolioPage ({}) {
+    return (
+        <main>
+            <h1>User Portfolio</h1>
+        </main>
+    )
+}
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,16 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import Link from 'next/link'
 
 export default function Home() {
   return (
     <div>
-      
+      <Link prefetch={false} href={{
+        pathname: '/[username]',
+        query: { username: 'avblackwell'},
+      }}>
+      <a>Alex's profile</a>
+      </Link>
     </div>
-  )
+  );
 }
